4744fcc4a17571ed1f2e0892efb970405c4428e8
PhilemonVanDussen <vandussenph709@northwested.org>
2024-09-20 13:42:42 -0400

    Update concat-check.py

--- concat-check.py
+++ concat-check.py
@@ -1,1 +1,19 @@
+# PJ VanDussen
+# 9/20/2024
+# Concatenation Check
 
+# Task 1
+f_name = 'PJ' 
+fav_game = 'Super Smash Bros'
+print(f_name + ' loves to play ' + fav_game + ' because it is a really good game.')
+
+# Task 2
+print('What I learn about concatenation: ')
+print('How to use concatenation by using the + symbol to join strings together. ')
+print('To know that concatenation has its downsides to it like having to use mutiple quotation marks and keep on having to use the + symbol for longer strings. ')
+
+# Task 3 
+food_iteam = 'Whopper'
+cost = 6.99 
+message = 'The cost of a    ' + food_iteam + ' for Burger King is $' + str(cost) + '.'
+print(message)
